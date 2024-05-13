e8f4a718bcaf7aab1b5261eb73fb07522bccae09
Harshdevloper123 <159701917+Harshdevloper123@users.noreply.github.com>
2024-05-13 20:00:17 +0530

    Add files via upload

---
+++ agefinder.js
@@ -0,0 +1,6 @@
+console.log("Age finder code is 2252")
+var a =prompt("enter your birth year")
+var cr = 2024
+var res = cr - a   
+
+confirm("you are currently in path of completig "+ res +" years") 
