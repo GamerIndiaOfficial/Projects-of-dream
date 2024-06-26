54849772955d6b43e9626a9afcb82a7cbe112006
GamerIndia <155443275+GamerIndiaOfficial@users.noreply.github.com>
2024-06-26 13:39:28 +0530

    Update agefinder.js

--- agefinder.js
+++ agefinder.js
@@ -1,6 +1,6 @@
 console.log("Age finder code is 2252")
-var a =prompt("enter your birth year")
+var a =prompt("Enter your birth year")
 var cr = 2024
 var res = cr - a   
 
-confirm("you are currently in path of completig "+ res +" years") 
+confirm("You are currently "+ res +" years") 
